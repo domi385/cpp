992d8d86bf2a2f75a43c82f3fd7036246a664b63
Dominik Ładnowski <dominik.ladnowski@outlook.com>
2024-10-05 16:51:10 +0200

    Dodanie lekcji 13

--- kursy/CS_gra/CS_gra.cpp
+++ kursy/CS_gra/CS_gra.cpp
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿#include <iostream>
+#include <iostream>
 
 using namespace std;
 
@@ -58,65 +57,4 @@ int main() {
 	}
 
 	return 0;
-=======
-﻿#include <iostream>
-
-using namespace std;
-
-int win, lose;
-
-void sprawdz_staty() {
-
-	cout << "Wygrane: " << win << endl;
-	cout << "Przegrane: " << lose << endl;
-
-}
-
-void wygranaGra() {
-
-	win++;
-	sprawdz_staty();
-
-}
-
-void przegranaGra() {
-
-	lose++;
-	sprawdz_staty();
-
-}
-
-int main() {
-	int wybor;
-	while (true) {
-
-		cout << "\n1. Wygranlem mecz,\n2. Przegralem mecz,\n3. Pokaz staty,\nCo chcesz zrobic: ";
-		cin >> wybor;
-
-		switch (wybor) {
-
-		case 1:
-			system("CLS");
-			wygranaGra();
-			break;
-
-		case 2:
-			system("CLS");
-			przegranaGra();
-			break;
-
-		case 3:
-			system("CLS");
-			sprawdz_staty();
-			break;
-
-		default:
-			system("CLS");
-			cout << "Nie ma takiej opcji.";
-			break;
-		}
-	}
-
-	return 0;
->>>>>>> 3c811f799e10910f5caaadecd103774e6d0ba59f
 }
---
+++ kursy/lekcja13/lekcja13.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	/*
+		Stwórz 5 elementową tablicę intów i wypisz jej elementy.
+	*/
+
+	int tablica [ 5 ] = { 1, 2, 3, 4, 5 };
+
+	cout << tablica [ 0 ] << endl;
+	cout << tablica [ 1 ] << endl;
+	cout << tablica [ 2 ] << endl;
+	cout << tablica [ 3 ] << endl;
+	cout << tablica [ 4 ] << endl;
+
+	cout << "=====" << endl;
+
+	for ( int i = 0; i < 5; i++ ) {
+
+		cout << tablica [ i ] << endl;
+	}
+
+	cout << "=====" << endl;
+
+	for ( int liczba : tablica ) {
+
+		cout << liczba << endl;
+	}
+
+	cout << "==========" << endl;
+
+	int kasa,poziom,ranga;
+
+	cin >> kasa >> poziom >> ranga;
+	if(kasa>=1000 && poziom>=100 && ranga>= 10){
+		cout << "Mozesz kupic railguna";
+	}else if(kasa>= 1000 || poziom >= 100 || ranga >= 10){
+		cout << "Niewiele Ci brakuje jeszce troche i bedzie Cie stac";
+	}else{
+		cout << "Jeszcze daleka droga" << endl;
+	}
+
+
+	cout << "==========" << endl;
+
+	/*
+	Zadanie 3 - Operacje matematyczne
+	Napisz program obliczający pole trapezu o bokach 4 i 13, oraz wysokości 7
+	P = (a + b)*h*1/2
+	*/
+
+	int a = 4 , b = 13 , h = 7;
+	
+	float P = ( ( a + b ) * h ) / 2.0;
+
+	cout << "Wynik: " << P << endl;
+
+	cout << "==========" << endl;
+
+	/*
+	Zadanie 5 -Pętle
+	Napisz pętlę liczącą od 20 do 1, wypisującą tylko liczby parzyste
+	*/
+
+	for ( int i = 20; i >= 1; i--  /*lub i -= 2 bez if'a*/ ) {
+
+		if ( i % 2 == 0 ) {
+			cout << i << endl;
+		}
+
+	}
+
+	return 0;
+}
+
